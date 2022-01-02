072d0ee4a3e425691f5d797a62af3ff7f2962c2b
tonmoysaha0005 <tonmoy.saha@gtechcp.com>
2022-01-02 15:02:54 +0600

    init

---
+++ Course App/Course-App/src/main/java/com/course/entity/Course.java
@@ -0,0 +1,50 @@
+package com.course.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String courseName;
+    private String authorName;
+
+
+    public Course() {
+    }
+
+    public Course(String courseName, String authorName) {
+        this.courseName = courseName;
+        this.authorName = authorName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
+
