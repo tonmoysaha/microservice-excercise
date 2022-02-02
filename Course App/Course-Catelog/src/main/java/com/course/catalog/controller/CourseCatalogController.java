490ad84e0d8d834973d4826887330c38484bc262
tonmoysaha0005 <tonmoy.saha@gtechcp.com>
2022-02-02 12:35:07 +0600

    circuit bracker

--- Course App/Course-Catelog/src/main/java/com/course/catalog/controller/CourseCatalogController.java
+++ Course App/Course-Catelog/src/main/java/com/course/catalog/controller/CourseCatalogController.java
@@ -3,6 +3,7 @@ package com.course.catalog.controller;
 import com.course.catalog.entity.Course;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -10,23 +11,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class CourseCatalogController {
 
+    private static final String COURSE_CATALOG = "courseCatalogService";
+
     @Autowired
     private EurekaClient eurekaClient;
 
+    @GetMapping("/home")
+    public String Home(){
+        return "exchange.getBody()";
+    }
+
+    /**
+     * fallback back method and method retun type should be same
+     * method signature should same except the exception parameter
+     * COURSE_CATALOG are circuitBreaker instances
+     * @return
+     */
     @GetMapping()
-    public List<Course> getAllCourses(){
+    @CircuitBreaker(name = COURSE_CATALOG, fallbackMethod = "tryAfterSomeTime")
+    public ResponseEntity<?> getAllCourses(){
         InstanceInfo nextServerFromEureka = eurekaClient.getNextServerFromEureka("course-app", false);
         String homePageUrl = nextServerFromEureka.getHomePageUrl();
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<List<Course>> exchange = restTemplate.exchange(homePageUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<Course>>() {
         });
 //        ResponseEntity<Course[]> exchange = restTemplate.exchange(homePageUrl, HttpMethod.GET, requestEntity, Course[].class);
-        return exchange.getBody();
+//        return ResponseEntity.ok(exchange.getBody());
+        return ResponseEntity.ok(exchange.getBody());
+    }
+    
+    public ResponseEntity<?> tryAfterSomeTime( Exception e){
+        return ResponseEntity.ok("no data found");
     }
 
     @PostMapping()
